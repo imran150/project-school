1324034462bd2fe127ed572150609a8db7cef90b
imran150 <mahmud.bubt.150@gmail.com>
2020-07-06 06:49:22 -0600

    Log In page done(Bootstrap)

--- index.php
+++ index.php
@@ -5,14 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Log In</title>
+    <link rel=" shortcut icon" type="image/x-icon" href="assets/img/icon/profile.png">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
 </head>
 <body>
     <div class="container">
-        <div class="card w-50 mx-auto mt-5">
+        <div class="card w-25 mx-auto mt-5">
             <div class="card-header">
                 <h4>Log In</h4>
+            </div>
+            <div class="card-body">
+                <form action="">
+                    <div class="form-group">
+                    <label for="">Email/Username</label>
+                    <input class="form-control" type="text" name="e_username" placeholder="Email/Username">
+                    </div>
+                    <div class="form-group">
+                    <label for="">Password</label>
+                    <input class="form-control" type="password" name="e_username" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                    <input class="btn btn-primary btn-block" type="submit" value="Log In">
+                    </div>
+                </form>
             </div>
         </div>
     </div>
